1adaf1b30a2a2b668b3957801d4a030e8007cb1d
tsonga <abstsonga@gmail.com>
2024-09-08 12:49:09 +0500

    feat: populate games ui

--- src/components/pages/games/index.tsx
+++ src/components/pages/games/index.tsx
@@ -6,8 +6,20 @@ import { useGetAllGames } from "~/components/shared/lib/hooks/games";
 import { GameCreateForm } from "~/components/widgets/forms/games/game-create-form";
 import { CreateButton } from "~/components/entities/create-button";
 import { PageContainer } from "~/components/shared/ui";
-import { Heading1, Heading3, Paragraph } from "~/components/shared/ui/typography";
+import {
+  Heading1,
+  Heading3,
+  Paragraph,
+} from "~/components/shared/ui/typography";
 import Image from "next/image";
+
+function extractUppercase(text: string): string {
+  return text
+    .split(" ")
+    .filter((word) => /^[A-Z]/.test(word))
+    .map((word) => word[0])
+    .join("");
+}
 
 export const GamesPage = () => {
   const [createGameToggle, setCreateGameToggle] = useState(false);
@@ -42,21 +54,36 @@ export const PopulateGames = () => {
   if (games?.length === 0) return <Paragraph>No games found.</Paragraph>;
 
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-6 md:gap-8 md:grid-cols-2 lg:grid-cols-3">
       {games?.map((game) => (
-        <div key={game.id} className="flex flex-row gap-4 p-4">
-          <div className="flex flex-row items-center justify-center">
-          <Image src={game.home_team.image ?? ""} width={32} height={32} alt={`Image of ${game.home_team.name}`} />
-          <Paragraph>
-            {game.home_team.name}
-          </Paragraph>
+        <div
+          key={game.id}
+          className="flex w-full flex-row items-center justify-center gap-4 rounded-xl bg-[#E8BF77] p-2 text-black shadow-md font-bold"
+        >
+          <div className="flex flex-row items-center justify-center gap-4 text-right">
+            <p className="text-[14px]">
+              {extractUppercase("School of Mining and Geosciences")}
+            </p>
+            <Image
+              src={game.home_team.image ?? ""}
+              width={40}
+              height={40}
+              className="h-10 w-10 rounded-full object-cover"
+              alt={`Image of ${game.home_team.name}`}
+            />
           </div>
           <Heading3>VS</Heading3>
-          <div className="flex flex-row items-center justify-center">
-          <Image src={game.away_team.image ?? ""} width={32} height={32} alt={`Image of ${game.away_team.name}`} />
-          <Paragraph>
-            {game.away_team.name}
-          </Paragraph>
+          <div className="flex flex-row items-center justify-center gap-4 text-left">
+            <Image
+              src={game.away_team.image ?? ""}
+              width={40}
+              height={40}
+              className="h-10 w-10 rounded-full object-cover"
+              alt={`Image of ${game.away_team.name}`}
+            />
+            <p className="text-[14px]">
+              {extractUppercase("Social Sciences and Humanities")}
+            </p>
           </div>
         </div>
       ))}
