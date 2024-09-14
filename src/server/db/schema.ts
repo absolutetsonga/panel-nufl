844ae48a6dde26180e45e292b4eeeb15866540fe
tsonga <abstsonga@gmail.com>
2024-09-14 17:38:40 +0500

    feat: cascade deletion references added

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -28,7 +28,9 @@ export const teams = createTable(
   {
     id: serial("id").primaryKey(),
     user_id: varchar("user_id", { length: 256 }).notNull(),
-    tournament_id: integer("tournament_id").notNull(),
+    tournament_id: integer("tournament_id")
+      .notNull()
+      .references(() => tournaments.id, { onDelete: "cascade" }),
     name: varchar("name", { length: 256 }).notNull(),
     image: varchar("image", { length: 1024 }),
     createdAt: timestamp("created_at", { withTimezone: true })
@@ -45,7 +47,9 @@ export const teams = createTable(
 
 export const players = createTable("player", {
   id: serial("id").primaryKey(),
-  team_id: integer("team_id").notNull(),
+  team_id: integer("team_id")
+    .notNull()
+    .references(() => teams.id, { onDelete: "cascade" }),
   user_id: varchar("user_id", { length: 256 }).notNull(),
 
   fullname: varchar("fullname", { length: 256 }).notNull(),
@@ -75,7 +79,9 @@ export const players = createTable("player", {
 
 export const gameweeks = createTable("gameweek", {
   id: serial("id").primaryKey(),
-  tournament_id: integer("tournament_id").notNull(),
+  tournament_id: integer("tournament_id")
+    .notNull()
+    .references(() => tournaments.id, { onDelete: "cascade" }),
   user_id: varchar("user_id", { length: 256 }).notNull(),
   number: integer("number").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
@@ -89,9 +95,15 @@ export const gameweeks = createTable("gameweek", {
 export const games = createTable("game", {
   id: serial("id").primaryKey(),
 
-  gameweek_id: integer("gameweek_id").notNull(),
-  home_team_id: integer("home_team_id").notNull(),
-  away_team_id: integer("away_team_id").notNull(),
+  gameweek_id: integer("gameweek_id")
+    .notNull()
+    .references(() => gameweeks.id, { onDelete: "cascade" }),
+  home_team_id: integer("home_team_id")
+    .notNull()
+    .references(() => teams.id, { onDelete: "cascade" }),
+  away_team_id: integer("away_team_id")
+    .notNull()
+    .references(() => teams.id, { onDelete: "cascade" }),
   user_id: varchar("user_id", { length: 256 }).notNull(),
 
   venue: varchar("venue", { length: 256 }).notNull(),
@@ -113,9 +125,15 @@ export const goals = createTable("goal", {
   id: serial("id").primaryKey(),
 
   user_id: varchar("user_id", { length: 256 }).notNull(),
-  game_id: integer("game_id").notNull(),
-  team_id: integer("team_id").notNull(),
-  player_id: integer("player_id").notNull(),
+  game_id: integer("game_id")
+    .notNull()
+    .references(() => games.id, { onDelete: "cascade" }),
+  team_id: integer("team_id")
+    .notNull()
+    .references(() => teams.id, { onDelete: "cascade" }),
+  player_id: integer("player_id")
+    .notNull()
+    .references(() => players.id, { onDelete: "cascade" }),
 
   is_own_goal: boolean("is_own_goal").default(false),
 
@@ -130,10 +148,18 @@ export const goals = createTable("goal", {
 export const assists = createTable("assist", {
   id: serial("id").primaryKey(),
   user_id: varchar("user_id", { length: 256 }).notNull(),
-  game_id: integer("game_id").notNull(),
-  player_id: integer("player_id").notNull(),
-  goal_id: integer("goal_id").notNull(),
-  team_id: integer("team_id").notNull(),
+  game_id: integer("game_id")
+    .notNull()
+    .references(() => games.id, { onDelete: "cascade" }),
+  player_id: integer("player_id")
+    .notNull()
+    .references(() => players.id, { onDelete: "cascade" }),
+  goal_id: integer("goal_id")
+    .notNull()
+    .references(() => goals.id, { onDelete: "cascade" }),
+  team_id: integer("team_id")
+    .notNull()
+    .references(() => teams.id, { onDelete: "cascade" }),
 
   createdAt: timestamp("created_at", { withTimezone: true })
     .default(sql`CURRENT_TIMESTAMP`)
@@ -146,10 +172,15 @@ export const assists = createTable("assist", {
 export const clean_sheets = createTable("clean_sheet", {
   id: serial("id").primaryKey(),
   user_id: varchar("user_id", { length: 256 }).notNull(),
-  game_id: integer("game_id").notNull(),
-  player_id: integer("player_id").notNull(),
-  team_id: integer("team_id").notNull(),
-
+  game_id: integer("game_id")
+    .notNull()
+    .references(() => games.id, { onDelete: "cascade" }),
+  player_id: integer("player_id")
+    .notNull()
+    .references(() => players.id, { onDelete: "cascade" }),
+  team_id: integer("team_id")
+    .notNull()
+    .references(() => teams.id, { onDelete: "cascade" }),
   createdAt: timestamp("created_at", { withTimezone: true })
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
@@ -161,10 +192,15 @@ export const clean_sheets = createTable("clean_sheet", {
 export const cards = createTable("card", {
   id: serial("id").primaryKey(),
   user_id: varchar("user_id", { length: 256 }).notNull(),
-  game_id: integer("game_id").notNull(),
-  player_id: integer("player_id").notNull(),
-  team_id: integer("team_id").notNull(),
-
+  game_id: integer("game_id")
+    .notNull()
+    .references(() => games.id, { onDelete: "cascade" }),
+  player_id: integer("player_id")
+    .notNull()
+    .references(() => players.id, { onDelete: "cascade" }),
+  team_id: integer("team_id")
+    .notNull()
+    .references(() => teams.id, { onDelete: "cascade" }),
   is_yellow: boolean("is_yellow").notNull(),
 
   createdAt: timestamp("created_at", { withTimezone: true })
